a27b58e1d59502d922fb2e2e42efa8199f26be4e
Eldho K Paul <eldhokpaul007@gmail.com>
2022-01-24 00:10:02 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div v-for="(item, i) in videoList" :key="i">
+      <VideoJS :src="item.videoUrl" :type="item.type" />
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoJS from "./components/VideoPlayer.vue";
+
+export default {
+  name: "App",
+  components: {
+    VideoJS,
+  },
+  data() {
+    return {
+      videoList: [
+        {
+          videoUrl:
+            "https://ov-cms.s3.ap-south-1.amazonaws.com/B2B/AdminPanel/CMS/AGY20519/Images/28092020061954.mp4",
+          type: "video/mp4",
+        },
+        {
+          videoUrl: "https://www.youtube.com/watch?v=DH1tVM4yOcU",
+          type: "video/youtube",
+        },
+        {
+          videoUrl: "https://www.youtube.com/watch?v=3OhLUYNw0TI",
+          type: "video/youtube",
+        },
+        {
+          videoUrl: "https://www.youtube.com/watch?v=CQaDl9Fu0W0",
+          type: "video/youtube",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <div class="videojs">
+      <div class="video-inner-container">
+        <video
+          id="vid1"
+          controls
+          preload="auto"
+          width="640"
+          height="264"
+          class="video-js vjs-fluid vjs-default-skin vjs-big-play-centered"
+          data-setup="{}"
+        >
+          <source :src="src" :type="type" />
+        </video>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "videojs-youtube/dist/Youtube.min.js";
+
+import "video.js/dist/video-js.min.css";
+
+import "video.js/dist/video.min.js";
+
+export default {
+  name: "VideoJS",
+  props: ["src", "type"],
+};
+</script>
